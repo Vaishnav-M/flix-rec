bbb4af24d40ae6e3e8e87fb0ca39a30a7798f7f9
Vaishnav-M <kichu.vyshnav@gmail.com>
2026-07-23 19:38:29 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import Row from './components/Row'
 import movies from './data/movies'
 
 export default function App() {
+  const { featured, trending, topRated, action, comedy } = movies
+
+  const withoutFeatured = (list) => list.filter((m) => m.id !== featured.id && m.title !== featured.title)
+
   return (
     <div className="app">
       <Navbar />
-      <Banner movie={movies.featured} />
+      <Banner movie={featured} />
       <main className="content">
-        <Row title="Trending Now" movies={movies.trending} />
-        <Row title="Top Rated" movies={movies.topRated} />
-        <Row title="Action Movies" movies={movies.action} />
-        <Row title="Comedy Movies" movies={movies.comedy} />
+        <Row title="Trending Now" movies={withoutFeatured(trending)} />
+        <Row title="Top Rated" movies={withoutFeatured(topRated)} />
+        <Row title="Action Movies" movies={withoutFeatured(action)} />
+        <Row title="Comedy Movies" movies={withoutFeatured(comedy)} />
       </main>
     </div>
   )
